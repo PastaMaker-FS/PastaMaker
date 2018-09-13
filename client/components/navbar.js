342dfe8fa4b9b6639c7574c4126e04c959f6e7ee
Kevin Duarte <duartekevin91@gmail.com>
2018-09-13 11:16:55 -0400

    Added buttons to navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,30 +3,109 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import {withStyles} from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>BOILERMAKER</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit
+  },
+  input: {
+    display: 'none'
+  }
+})
+
+const Navbar = ({handleClick, isLoggedIn, classes}) => {
+  return (
+    <div>
+      <h1>PastaMaker</h1>
+      <nav>
+        {isLoggedIn ? (
+          <div>
+            {/* The navbar will show these links after you log in */}
+            <Link to="/home">
+              <Button
+                variant="outlined"
+                color="primary"
+                className={classes.button}
+              >
+                Home
+              </Button>
+            </Link>
+            <Button
+              variant="outlined"
+              color="primary"
+              className={classes.button}
+            >
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+            </Button>
+            <Link to="/user">
+              <Button
+                variant="outlined"
+                color="primary"
+                className={classes.button}
+              >
+                User Info
+              </Button>
+            </Link>
+            <Link to="/aboutus">
+              <Button
+                variant="outlined"
+                color="primary"
+                className={classes.button}
+              >
+                About Us
+              </Button>
+            </Link>
+          </div>
+        ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Link to="/home">
+              <Button
+                variant="outlined"
+                color="primary"
+                className={classes.button}
+              >
+                Home
+              </Button>
+            </Link>
+            <Link to="/login">
+              <Button
+                variant="outlined"
+                color="primary"
+                className={classes.button}
+              >
+                Login
+              </Button>
+            </Link>
+            <Link to="/signup">
+              <Button
+                variant="outlined"
+                color="primary"
+                className={classes.button}
+              >
+                Sign Up
+              </Button>
+            </Link>
+            <Link to="/aboutus">
+              <Button
+                variant="outlined"
+                color="primary"
+                className={classes.button}
+              >
+                About Us
+              </Button>
+            </Link>
+          </div>
+        )}
+      </nav>
+      <hr />
+    </div>
+  )
+}
 
 /**
  * CONTAINER
@@ -45,12 +124,13 @@ const mapDispatch = dispatch => {
   }
 }
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(withStyles(styles)(Navbar))
 
 /**
  * PROP TYPES
  */
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
+  isLoggedIn: PropTypes.bool.isRequired,
+  classes: PropTypes.object.isRequired
 }
