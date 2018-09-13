bc1d1af6afa8fde986667a766c79923267fdd021
Kevin Duarte <duartekevin91@gmail.com>
2018-09-13 14:04:44 -0400

    Finished all products

--- client/app.js
+++ client/app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import {Navbar} from './components'
+import AllProducts from './components/allProducts'
 import Routes from './routes'
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <Navbar />
       <Routes />
+      <AllProducts />
     </div>
   )
 }
--- client/routes.js
+++ client/routes.js
@@ -29,7 +29,7 @@ class Routes extends Component {
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={UserHome} />
+            <Route path="/user" component={UserHome} />
             <Route path="/cart" component={CartView} />
           </Switch>
         )}
